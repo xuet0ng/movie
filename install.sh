087cbbce8ada81a6baf6046708e63f26380cc8b5
xuet0ng <ttslj@163.com>
2016-11-22 09:45:39 +0000

    fix bug

--- install.sh
+++ install.sh
@@ -4,5 +4,9 @@
 mvn package
 
 # deploy
-ps -ef | grep java | grep movie | grep -v grep| awk '{print $2}' | xargs kill -9
+
+for pid in $(ps -ef | grep java | grep movie | grep -v grep| awk '{print $2}'); do
+  kill -9 $pid
+done
+
 screen -dm java -jar target/movie-0.0.1-SNAPSHOT.jar
